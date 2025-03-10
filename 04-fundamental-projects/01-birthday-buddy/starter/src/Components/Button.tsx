faef7ab59fa0bb7ffc4e8b95f0221ca6d9f55681
blackmamba17 <scuolatorrisi@gmail.com>
2025-03-10 23:22:57 +0100

    finished even the logic, first project is done

--- 04-fundamental-projects/01-birthday-buddy/starter/src/Components/Button.tsx
+++
@@ -1,3 +0,0 @@
-import React, { FC } from "react";
-
-export const Button: FC<{ buttonText: string }> = ({ buttonText }) => <button className="btn" style={{width:""}}>{ buttonText}</button>
--- 04-fundamental-projects/01-birthday-buddy/starter/src/Components/Ulist.tsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/Components/Ulist.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import listOfPersons from '../data'
+import React, { FC } from 'react'
 
-export const Ulist = () => {
+export const Ulist: FC<{
+    getListOfPersons: {
+        id: number;
+        name: string;
+        age: number;
+        image: string;
+    }[]
+}> = ({ getListOfPersons }) => {
     return (
-    <ul style={{paddingTop: "1rem"}}>
-        {listOfPersons.map((person, index) => {
-            return (
-                <li key={person.name+person.id}>
-                    <article className='personArticle'>
-                        <img className='listOfImages' src={person.image} />
-                        <p style={{display:"inline-block"}}><h3>{person.name}</h3><br /><h4>{ person.age} years</h4></p>
-                    </article>
-                </li>
-            )
-        })}
-    </ul>)
+        <>
+            <ul style={{ paddingTop: "1rem" }}>
+                {getListOfPersons.map((person, index) => {
+                    return (
+                        <li key={person.name + person.id}>
+                            <article className='personArticle'>
+                                <img className='listOfImages' src={person.image} />
+                                <p style={{ display: "inline-block" }}><h3>{person.name}</h3><br /><h4>{person.age} years</h4></p>
+                            </article>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>)
 }
