502b68e83f3ab272251c9c17d56bf501b537b742
blackmamba17 <scuolatorrisi@gmail.com>
2025-03-24 22:34:57 +0100

    fe

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -19,20 +19,7 @@ const CleanupFunction = () => {
   return (
     <>
       test hevc
-      <script type="module" src="https://cdn.jsdelivr.net/npm/media-chrome@4/+esm"></script>
-      <media-controller>
-        <video slot="media" src="https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4" crossorigin>
-          <track label="thumbnails" default kind="metadata" src="https://image.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/storyboard.vtt" />
-        </video>
-        <media-control-bar>
-          <media-play-button></media-play-button>
-          <media-mute-button></media-mute-button>
-          <media-volume-range></media-volume-range>
-          <media-time-range></media-time-range>
-          <media-pip-button></media-pip-button>
-          <media-fullscreen-button></media-fullscreen-button>
-        </media-control-bar>
-      </media-controller>
+
     </>
   )
 };
