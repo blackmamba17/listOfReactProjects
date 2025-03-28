c0965583b2020c0772f07d6cbde80dbb64a65ae8
blackmamba17 <scuolatorrisi@gmail.com>
2025-03-29 00:24:34 +0100

    WIP: improving

--- 04-fundamental-projects/04-accordion/starter/src/App.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/App.jsx
@@ -5,7 +5,7 @@ import { questions } from './data'
 const App = () => {
   return (
     <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-      <div style={{ backgroundColor: "white", width: "80%", height: "80vh", display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "center", borderRadius: "1rem", boxShadow: "0.5rem 0.5rem 0.5rem", fontSize: "1rem" }}>
+      <div style={{ backgroundColor: "white", width: "80vw", height: "80vh", display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "center", borderRadius: "1rem", boxShadow: "0.5rem 0.5rem 0.5rem", fontSize: "1rem" }}>
         <Title title="Questions" />
         <Question questions={questions} />
       </div>
