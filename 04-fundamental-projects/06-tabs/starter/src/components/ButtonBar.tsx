a5e4a440bc7f8fe36c8dd04c9f3427b22f7a62dd
gtorrisi <gtorrisi@movia.biz>
2025-04-04 17:00:43 +0200

    project finished

--- 04-fundamental-projects/06-tabs/starter/src/components/ButtonBar.tsx
+++ 04-fundamental-projects/06-tabs/starter/src/components/ButtonBar.tsx
@@ -8,17 +8,19 @@ export const ButtonBar: FC<{
     labels: string[] | undefined,
     setIndexTab: React.Dispatch<React.SetStateAction<number>>
 }> = ({ labels, setIndexTab }) => {
+    const [isActive, setIsActive] = useState(0)
 
     return <div style={{ display: "flex", margin: "3rem", flexDirection: "column" }}>
         {labels?.map((label, index) => {
             const [isHover, setIsHover] = useState(false)
             return <a onClick={() => {
                 setIndexTab(index)
+                setIsActive(index)
             }} onMouseEnter={() => {
                 setIsHover(!isHover)
             }} onMouseLeave={() => {
                 setIsHover(!isHover)
-            }} key={'button bar ' + index} style={{ textDecoration: isHover ? 'underline' : '', margin: "1rem", cursor: "pointer", color: isHover ? "green" : "black" }}>{label}</a>
+            }} key={'button bar ' + index} style={{ textDecoration: isHover ? 'underline' : isActive === index ? 'underline' : '', margin: "1rem", cursor: "pointer", color: isHover ? "green" : isActive === index ? 'green' : 'black' }}>{label}</a>
         })}
     </div>
 }
