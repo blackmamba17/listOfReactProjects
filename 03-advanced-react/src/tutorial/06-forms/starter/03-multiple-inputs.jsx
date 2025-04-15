c7980c86c26e900541b1fc3a67ed3a9715b59c09
blackmamba17 <scuolatorrisi@gmail.com>
2025-04-15 23:44:56 +0200

    devs

--- 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
+
 const MultipleInputs = () => {
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(user);
+
+  }
+
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Multiple Inputs</h4>
         {/* name */}
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input onChange={handleChange} type='text' className='form-input' id='name' value={user.name} />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='email' className='form-label'>
             Email
           </label>
-          <input type='email' className='form-input' id='email' />
+          <input onChange={handleChange} type='email' className='form-input' id='email' value={user.email} />
         </div>
         {/* email */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
           </label>
-          <input type='password' className='form-input' id='password' />
+          <input onChange={handleChange} type='password' className='form-input' id='password' value={user.password} />
         </div>
 
         <button type='submit' className='btn btn-block'>
--- 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false)
+  const handleShipping = (e) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked)
+
+  }
+
   return (
-    <div>
+    < div >
       <form className='form'>
         <h4>Other Inputs</h4>
         {/* name */}
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='shipping'> Free Shipping </label>
+          <input onChange={handleShipping} type="checkbox" name="shipping" id="shipping" />
         </div>
         <div className='form-row' style={{ textAlign: 'left' }}>
           <label htmlFor='framework' className='form-label'>
